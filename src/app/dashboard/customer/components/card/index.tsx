e67258d50804144a0fd8a16af0450486e09de0b3
mark <mark.sva@hotmail.com>
2025-10-20 11:50:46 -0300

    search customers list

--- src/app/dashboard/customer/components/card/index.tsx
+++ src/app/dashboard/customer/components/card/index.tsx
@@ -1,12 +1,14 @@
-export function CardCustomer() {
+import { CustomerProps } from "@/utils/customer.type"
+
+export function CardCustomer({ customer }: { customer: CustomerProps }) {
 
     return (
         <article className="flex flex-col bg-gray-100 border-1 border-gray-200 p-2 rounded-lg gap-2 hover:scale-103 duration-200">
             <h2>
-                <a className="font-bold">Nome:</a> Mercado Silva
+                <a className="font-bold">Nome:</a> {customer.name}
             </h2>
-            <p><a className="font-bold">Email:</a> teste@teste.com</p>
-            <p><a className="font-bold">Telefone:</a> XX99101010</p>
+            <p><a className="font-bold">Email:</a> {customer.email}</p>
+            <p><a className="font-bold">Telefone:</a> {customer.phone}</p>
             <button className="bg-red-500 px-3 rounded text-white mt-2 self-start">
                 Deletar
             </button>
