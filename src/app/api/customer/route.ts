13d91f4ab69a5509eb1c7023b8b77d9b424f833f
mark <mark.sva@hotmail.com>
2025-10-20 12:22:18 -0300

    implement delete customer

--- src/app/api/customer/route.ts
+++ src/app/api/customer/route.ts
@@ -3,6 +3,47 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 import prismaClient from "@/lib/prisma";
 
+export async function DELETE(request: Request) {
+
+    const session = await getServerSession(authOptions);
+
+    if (!session || !session.user) {
+        return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+
+    const { searchParams } = new URL(request.url);
+    const userId = searchParams.get("id");
+
+    if (!userId) {
+        return NextResponse.json({ message: "failed delete customer" }, { status: 400 });
+    }
+
+    const findTickets = await prismaClient.ticket.findFirst({
+        where: {
+            customerId: userId
+        }
+    })
+
+    if (findTickets) {
+        return NextResponse.json({ message: "cannot delete customer with tickets" }, { status: 400 });
+    }
+
+    try {
+        await prismaClient.customer.delete({
+            where: {
+                id: userId as string
+            }
+        })
+
+        return NextResponse.json({ message: "cliente deletado com sucesso" }, { status: 200 });
+
+    } catch (error) {
+        return NextResponse.json({ message: "failed delete customer" }, { status: 400 });
+    }
+
+
+}
+
 export async function POST(request: Request) {
     const session = await getServerSession(authOptions);
 
--- src/app/dashboard/customer/components/card/index.tsx
+++ src/app/dashboard/customer/components/card/index.tsx
@@ -1,6 +1,31 @@
+"use client"
+
 import { CustomerProps } from "@/utils/customer.type"
+import { api } from "@/lib/api"
+import { useRouter } from "next/navigation";
 
 export function CardCustomer({ customer }: { customer: CustomerProps }) {
+
+    const router = useRouter();
+
+    async function handleDeleteCustomer() {
+
+        try {
+            const response = await api.delete("/api/customer", {
+                params: {
+                    id: customer.id
+                }
+            });
+
+            if (response.status === 200) {
+                router.refresh();
+            }
+
+        } catch (error) {
+            console.error("Failed to delete customer", error);
+        }
+
+    }
 
     return (
         <article className="flex flex-col bg-gray-100 border-1 border-gray-200 p-2 rounded-lg gap-2 hover:scale-103 duration-200">
@@ -9,7 +34,10 @@ export function CardCustomer({ customer }: { customer: CustomerProps }) {
             </h2>
             <p><a className="font-bold">Email:</a> {customer.email}</p>
             <p><a className="font-bold">Telefone:</a> {customer.phone}</p>
-            <button className="bg-red-500 px-3 rounded text-white mt-2 self-start">
+            <button
+                className="bg-red-500 px-3 rounded text-white mt-2 self-start"
+                onClick={handleDeleteCustomer}
+            >
                 Deletar
             </button>
         </article>
