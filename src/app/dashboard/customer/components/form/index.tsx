7bedeb2bcdbd699ccee133e820869e33a1b19b4c
mark <mark.sva@hotmail.com>
2025-10-21 12:09:27 -0300

    fix bug create customer

--- src/app/dashboard/customer/components/form/index.tsx
+++ src/app/dashboard/customer/components/form/index.tsx
@@ -41,7 +41,7 @@ export function NewCustomerForm({ userId }: { userId: string }) {
             address: data.address,
             userId: userId
         })
-        setLoading(false);
+        
         router.refresh();
         router.replace("/dashboard/customer");
 
@@ -96,6 +96,7 @@ export function NewCustomerForm({ userId }: { userId: string }) {
             <button
                 type="submit"
                 className="bg-blue-500 my-4 px-2 h-11 rounded text-white font-bold"
+                disabled={loading}
             >
                 {loading ? (<div className='w-full flex items-center justify-center'><FiLoader size={26} className="animate-spin" /></div>) : "Cadastrar Cliente"}
             </button>
